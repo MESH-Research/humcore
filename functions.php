8c15064d85eca54e8837692cb3e511b6611b24d0
Ubuntu <ubuntu@hcommons.org>
2022-03-02 14:54:35 +0000

    admin-docs-support-150-give-user-deposit-for-others-permission

--- functions.php
+++ functions.php
@@ -1824,7 +1824,7 @@ function humcore_deposits_user_committee_list( $user_id ) {
  */
 function humcore_deposits_can_deposit_for_others( $user_id ) {
 
-	return in_array( $user_id, array( '60', '4381', '5610', '1027782', '1015176', '1020610', '1006552', '1018587' ) );
+	return in_array( $user_id, array( '60', '4381', '5610', '1027782', '1015176', '1020610', '1006552', '1018587', '1010049' ) );
 	return apply_filters( 'humcore_deposits_can_deposit_for_others', $user_id );
 
 }
